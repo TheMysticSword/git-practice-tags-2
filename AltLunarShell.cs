ad85e49702e83559b3ae731634c19d4315ff2040
David Galiev <galiev.david@yandex.ru>
2022-10-13 20:33:44 +0500

    Make LunarShell reduce dmg to 0 if it's less than 1% of HP

--- AltLunarShell.cs
+++ AltLunarShell.cs
@@ -67,6 +67,7 @@ namespace AspectAbilities.Buffs
             if (victimInfo.body && victimInfo.body.HasBuff(AspectAbilitiesContent.Buffs.AspectAbilities_AltLunarShell) && victimInfo.healthComponent)
             {
                 damage = Mathf.Min(damage, victimInfo.healthComponent.fullCombinedHealth * maxDamageTaken / 100f);
+                if ((damage / victimInfo.healthComponent.fullCombinedHealth) < 0.01f) damage = 0;
             }
         }
     }
